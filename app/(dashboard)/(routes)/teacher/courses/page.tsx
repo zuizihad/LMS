73e1e0e863c5158271b06043727b28e7d4aa6a1a
Zihadul Islam <ahmedxihad@gmail.com>
2023-09-23 11:31:50 +0600

    course creation page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const CoursePage = () => {
   return (
-    <div>CoursePage</div>
+    <div className='p-6'>
+      <Link href={`/teacher/create`}>
+        <Button>
+          New Course
+        </Button>
+      </Link>
+    </div>
   )
 }
 
