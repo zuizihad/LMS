a301d0a9d1a685f4f9cbf000deea0c951cdc832a
Zihadul Islam <ahmedxihad@gmail.com>
2023-09-30 10:51:13 +0600

    Implement category query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import React from "react";
+import Categories from "./_components/categories";
 
-const SearchPage = () => {
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
   return (
-    <div>SearchPage</div>
-  )
-}
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
