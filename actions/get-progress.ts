f2cf3cf6a94d04871371db51062c36115c19e303
Zihadul Islam <ahmedxihad@gmail.com>
2023-09-30 13:05:49 +0600

    Implement course grid view and get progress

---
+++ actions/get-progress.ts
@@ -0,0 +1,39 @@
+import { db } from "@/lib/db";
+import React from "react";
+
+export const getProgress = async (
+  userId: string,
+  courseId: string
+): Promise<number> => {
+  try {
+    const publishedChapters = await db.chapter.findMany({
+      where: {
+        courseId: courseId,
+        isPublished: true,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const publishedChapterIds = publishedChapters.map((chapter) => chapter.id);
+
+    const validCompletedChapters = await db.userProgress.count({
+      where: {
+        userId: userId,
+        chapterId: {
+          in: publishedChapterIds,
+        },
+        isCompleted: true,
+      },
+    });
+
+    const progressPercentage =
+      (validCompletedChapters / publishedChapterIds.length) * 100;
+    return progressPercentage;
+  } catch (error) {
+    console.log("[PROGRESS]", error);
+    return 0;
+  }
+};
+
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,20 +2,42 @@ import { db } from "@/lib/db";
 import React from "react";
 import Categories from "./_components/categories";
 import SearchInput from "@/components/search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getCourses } from "@/actions/get-courses";
+import CoursesList from "@/components/courses-list";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/");
+  }
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
   return (
     <>
       <div className="px-6 pt-6 md:hidden md: mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
