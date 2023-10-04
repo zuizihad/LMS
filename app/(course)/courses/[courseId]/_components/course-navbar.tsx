d27373ed869afb4a08634d02f245b1be0288384c
Zihadul Islam <ahmedxihad@gmail.com>
2023-10-04 18:47:32 +0600

    implement course and chapter layout and navbar

---
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -0,0 +1,27 @@
+import NavbarRoutes from "@/components/navbar-routes";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+import React from "react";
+import CourseMobileSidebar from "./course-mobile-sidebar";
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number | null;
+}
+
+const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <CourseMobileSidebar 
+        course={course}
+        progressCount={progressCount}
+      />
+      <NavbarRoutes />
+    </div>
+  );
+};
+
+export default CourseNavbar;
---
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+import { redirect } from "next/navigation";
+import React from "react";
+import CourseSidebarItem from "./course-sidebar-item";
+
+interface CourseSidebarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id,
+      },
+    },
+  });
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+      <div className="p-8 flex flex-col border-b">
+        <h1 className="font-semibold">{course.title}</h1>
+        {/* Check purchase and progress */}
+      </div>
+      <div className="flex flex-col w-full ">
+        {course.chapters.map((chapter) => (
+            <CourseSidebarItem 
+                key={chapter.id}
+                id={chapter.id}
+                label={chapter.title}
+                courseId={course.id}
+                isLocked={!chapter.isFree && !purchase}
+                isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CourseSidebar;
